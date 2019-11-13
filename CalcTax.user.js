dcf4783f2c4f0d4ab40d5db3f26e8f662dcaf141
xonix <xonixx@gmail.com>
2019-11-13 17:33:14 +0200

    initial

---
+++ CalcTax.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         CalcTax
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://24.privatbank.ua/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    alert('Hello');
+    // Your code here...
+})();
